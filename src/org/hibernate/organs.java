a39b533720688dd422c00c94e66fbea5cc6ad25b
Divya10 <divya.agarwal24@outlook.com>
2016-11-19 17:19:58 +0530

    commit 6

--- src/org/hibernate/organs.java
+++ src/org/hibernate/organs.java
@@ -2,12 +2,22 @@ package org.hibernate;
 import java.util.*;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 @Entity
 public class organs {
 	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	String id;
 	String UserId;
 	String Organ;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
 	public String getUserId() {
 		return UserId;
 	}
